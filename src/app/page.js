fd9d6a366930e80db16b4b3a80a46b6a13e80dc3
Rahul <rahulchhlotre18@gmail.com>
2025-10-08 19:36:34 +0530

    dynamic import of producttablecall

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,12 @@
 "use client";
+import dynamic from "next/dynamic";
 import Header from "./component/header/page";
 
-import ProductTableCall from "./component/product_table/producttable";
+// import ProductTableCall from "./component/product_table/producttable";
+const ProductTableCall = dynamic(
+  () => import("./component/product_table/producttable"),
+  { ssr: false }
+);
 
 export default function Home() {
   return (
